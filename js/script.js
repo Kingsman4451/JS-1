e487e4a15bcee02ea28c1c2cb7b4958d8816fb21
Kingsman4451 <dadosh4451@gmail.com>
2022-04-08 22:22:55 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+alert("Xush kelibsiz!");
+var firstName = prompt("Ismingizni kiriting");
+var lastName = prompt("Familiyangizni kiriting");
+var age = prompt("Yoshingizni kiriting");
+var job = prompt("Kim bo'lib ishlaysiz?");
+alert("Ma'lumotlar uchun rahmat!");
+
+console.log(`Ism: ${firstName} \nFamiliya: ${lastName} \nYosh: ${age} \nKasb: ${job}`);
